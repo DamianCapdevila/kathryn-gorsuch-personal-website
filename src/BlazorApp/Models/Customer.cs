bb16dbabf5b18b061f23380500582c1a3f21a515
Sergio Damian Capdevila <101021404+DamianCapdevila@users.noreply.github.com>
2024-03-09 18:02:43 +0000

    (feat): add logo for customer + modify portfolio page

--- src/BlazorApp/Models/Customer.cs
+++ src/BlazorApp/Models/Customer.cs
@@ -1,6 +1,6 @@
 namespace BlazorApp.Models;
 
-public class Project
+public class Customer
 {
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
